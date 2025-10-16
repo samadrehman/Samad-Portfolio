d6982bbc96fcc423be990fb2bb538cb4666bb657
samad <samadrehman550@gmail.com>
2025-10-16 17:58:21 +0530

    Fix bug

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Replace with your Google Apps Script Web App URL
 const API_URL = 'https://script.google.com/macros/s/AKfycby8XBVlPLiTdBid1jxRQpkl71i7aWlPmgBbGpWCYcRjK-190sU2obHKkVZRyxyzMpK4YA/exec';
 
 async function logVisitorIP() {
@@ -24,3 +23,32 @@ window.onload = logVisitorIP;
 
 
 <script src="https://kit.fontawesome.com/42c784f5cd.js" crossorigin="anonymous"></script>
+
+document.getElementById('subscribeForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const form = e.target;
+
+    if (!navigator.geolocation) {
+        alert("Geolocation not supported.");
+        return;
+    }
+
+    navigator.geolocation.getCurrentPosition(async (pos) => {
+        const latitude = pos.coords.latitude;
+        const longitude = pos.coords.longitude;
+
+        const data = new URLSearchParams(new FormData(form));
+        data.append("latitude", latitude);
+        data.append("longitude", longitude);
+
+        const res = await fetch("https://script.google.com/macros/s/AKfycbwOMDe4q70jFjA-MCO4jB8DvN0mQIt3MrniZ_9t3pknjb-gePXBvBrCQv_6JeZsPfEQpA/exec", {
+            method: "POST",
+            body: data
+        });
+        const text = await res.text();
+        alert(text);
+    }, (err) => {
+        // Optional: handle errors
+        // alert("Unable to get location: " + err.message);
+    }, {enableHighAccuracy: true});
+});
